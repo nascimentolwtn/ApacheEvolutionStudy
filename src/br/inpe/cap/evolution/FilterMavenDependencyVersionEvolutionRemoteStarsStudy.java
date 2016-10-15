215d90e85ac8269a63448d06d74475d964d65632
Luiz Wagner <nascimentolwtn@gmail.com>
2016-10-15 11:07:26 -0300

    Estudo para filtrar apenas projetos que utilizam Maven dos projetos com 1000 estrelas.

---
+++ src/br/inpe/cap/evolution/FilterMavenDependencyVersionEvolutionRemoteStarsStudy.java
@@ -0,0 +1,152 @@
+package br.inpe.cap.evolution;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+import br.com.metricminer2.MetricMiner2;
+import br.com.metricminer2.RepositoryMining;
+import br.com.metricminer2.Study;
+import br.com.metricminer2.filter.range.Commits;
+import br.com.metricminer2.persistence.csv.CSVFile;
+import br.com.metricminer2.scm.GitRemoteRepository;
+import br.inpe.cap.auxiliar.JoinSummaryCSV;
+import br.inpe.cap.auxiliar.MultipleCSVFile;
+
+public class FilterMavenDependencyVersionEvolutionRemoteStarsStudy implements Study {
+
+	private static final int THREADS_FOR_REPOSITORIES = 10;
+	private static final String FOUNTAIN_PATH = "fountain" + File.separator;
+	private static final String STUDY_TEMP_PATH = "E:\\metricminer-evolution-stars-NOBARE";
+
+	private static final String STUDY_LOG_PATH = "." + File.separator + "study" + File.separator + "filter_stars_02";
+	private static final String EVOLUTION_LOG_PATH = STUDY_LOG_PATH + File.separator + "evolutions";
+	
+	private static final String FILE_PREFIX = "dependency_filter-HOME";
+	private static final String EVOLUTION_SUMMARY_CSV = STUDY_LOG_PATH + File.separator + FILE_PREFIX + ".csv"; 
+
+	private static final File GITHUB_URLS_FILE = new File(FOUNTAIN_PATH+"java-stars.url");
+	private static final File GITHUB_DONE_FILE = new File(FOUNTAIN_PATH+"done-github_filter-stars_HOME.txt");
+	private static final File EXCEPTION_FILE = new File(FOUNTAIN_PATH+"exceptions-filter-stars_HOME.log");
+	
+	private static Logger log;
+	
+	public static void main(String[] args) throws Exception {
+		System.setProperty("logfilename", FILE_PREFIX + "_filter02");
+		log = Logger.getLogger(RepositoryMining.class);
+		
+		checkRequiredLogFilesAndDirectories();
+		
+		new MetricMiner2().start(new FilterMavenDependencyVersionEvolutionRemoteStarsStudy());
+		
+		JoinSummaryCSV.joinSummaryCSV(EVOLUTION_LOG_PATH, new File(EVOLUTION_LOG_PATH + "_joined.csv"));
+		
+		System.out.println("Finish!");
+	}
+	
+	public void execute() {
+		try {
+			
+			List<String> gitRepoUrl = getRepositoryExceptDoneUrls();
+
+			ExecutorService execRepos = Executors.newFixedThreadPool(THREADS_FOR_REPOSITORIES);
+			for(String url : gitRepoUrl) {
+				execRepos.submit(() -> 
+					doMining(url, STUDY_TEMP_PATH));
+			}
+		
+			execRepos.shutdown();
+			execRepos.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+			
+		} catch (Exception e) {
+			try {
+				String causeMessage = "";
+				if(e.getCause() != null)
+					causeMessage = e.getCause().getMessage();
+				FileUtils.writeStringToFile(EXCEPTION_FILE, e.getMessage() + ". Cause: " + causeMessage +"\n", true);
+				System.gc();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	private void doMining(String gitUrl, String tempDir) {
+		try {
+			String gitReposLogSubDir = gitUrl.substring(gitUrl.lastIndexOf("/")+1, gitUrl.length());
+			GitRemoteRepository gitRemoteRepository = GitRemoteRepository
+					.hostedOn(gitUrl)
+					.inTempDir(tempDir)
+					.withMaxNumberOfFilesInACommit(2000).build();
+			new RepositoryMining()
+				.in(gitRemoteRepository.info())
+				.startingFromTheBeginning()
+				.through(Commits.onlyInHead())
+				.process(new FilterMavenDependencyVisitor(EVOLUTION_LOG_PATH, gitReposLogSubDir), new MultipleCSVFile(
+						new CSVFile(EVOLUTION_SUMMARY_CSV, true)
+						,
+						new CSVFile(EVOLUTION_LOG_PATH
+								+ File.separator
+								+ "filter-stars-'"
+								+ gitReposLogSubDir
+								+ "'.csv")))
+				.mine();
+			markDone(gitUrl);
+			gitRemoteRepository.deleteTempGitPath();
+		} catch (Exception re) {
+			log.error(re.getMessage());
+		} finally {
+			System.gc();
+		}
+	}
+	
+	private List<String> getRepositoryExceptDoneUrls() throws IOException {
+		List<String> urls = FileUtils.readLines(GITHUB_URLS_FILE);
+		
+		FileReader arquivo = new FileReader(GITHUB_DONE_FILE);
+		BufferedReader reader = new BufferedReader(arquivo);
+
+		String linha = reader.readLine();
+		while (linha != null) {
+			urls.remove(linha);
+			linha = reader.readLine();
+		}
+
+		reader.close();
+		arquivo.close();
+		return urls;
+	}
+		
+	private void markDone(String gitUrl) {
+		try {
+			FileUtils.writeStringToFile(GITHUB_DONE_FILE, gitUrl + "\n", true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	private static void checkRequiredLogFilesAndDirectories() throws IOException {
+		if(!GITHUB_DONE_FILE.exists()) {
+			GITHUB_DONE_FILE.createNewFile();
+		}
+		
+		File studyPathDir = new File(STUDY_LOG_PATH);
+		if(!studyPathDir.exists()) {
+			studyPathDir.mkdir();
+		}
+		
+		File evolutionPathDir = new File(EVOLUTION_LOG_PATH);
+		if(!evolutionPathDir.exists()) {
+			evolutionPathDir.mkdir();
+		}
+	}
+
+}
---
+++ src/br/inpe/cap/evolution/FilterMavenDependencyVisitor.java
@@ -0,0 +1,81 @@
+package br.inpe.cap.evolution;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import org.apache.commons.io.FileUtils;
+
+import br.com.metricminer2.domain.Commit;
+import br.com.metricminer2.persistence.PersistenceMechanism;
+import br.com.metricminer2.scm.CommitVisitor;
+import br.com.metricminer2.scm.SCMRepository;
+import br.inpe.cap.evolution.domain.MavenDependency;
+import br.inpe.cap.evolution.domain.MavenProject;
+import br.inpe.cap.evolution.parser.XmlMavenParser;
+
+public class FilterMavenDependencyVisitor implements CommitVisitor {
+	
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private File pomDir;
+	
+	public XmlMavenParser parser = new XmlMavenParser();
+
+	private String repositoryName;
+
+	public FilterMavenDependencyVisitor(String evolutionLogPath, String gitReposLogSubDir) {
+		this.repositoryName = gitReposLogSubDir;
+		evolutionLogPath = "..\\..\\..\\..\\..\\" + evolutionLogPath; // comente esta linha para manter pom's dentro do diretório do estudo
+		this.pomDir = new File(evolutionLogPath + File.separator + gitReposLogSubDir);
+	}
+
+	@Override
+	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
+
+		repo.getScm().files().stream()
+			.filter(
+				(file) -> file.fileNameEndsWith("pom.xml"))
+			.forEach( 
+				(file) ->	{
+					String sourceCode = file.getSourceCode();
+					MavenProject pom = parser.readPOM(sourceCode);
+					pom.getDependencies().forEach(
+						(dependency) -> 
+							writeCsvLine(writer, commit, file.getFullName(), dependency)
+						);
+
+					try {
+						String pomFileName = pomDir.getAbsolutePath() + File.separator + file.getFile().getName();
+						FileUtils.writeStringToFile(new File(pomFileName), sourceCode);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+		);
+	
+	}
+
+	private void writeCsvLine(PersistenceMechanism writer, Commit commit, String fileName, MavenDependency mavenDependency) {
+		writer.write(
+				commit.getHash(),
+				DATE_FORMAT.format(commit.getDate().getTime()),
+				this.repositoryName,
+				fileName,
+				mavenDependency.getGroupId(),
+				mavenDependency.getArtifactId(),
+				mavenDependency.getVersion(),
+				replaceLineFeedAndComma(commit.getMsg())
+		);
+	}
+
+	private String replaceLineFeedAndComma(String commitMessage) {
+		return commitMessage.replaceAll("[\\r\\n;]+", "");
+	}
+
+	@Override
+	public String name() {
+		return "dependency-filter_" + this.repositoryName;
+	}
+
+}
