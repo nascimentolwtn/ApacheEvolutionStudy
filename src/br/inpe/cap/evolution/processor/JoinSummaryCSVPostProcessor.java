f2b1e6289812175c2b12a1ac3b93416b2e7d2637
Luiz Wagner <nascimentolwtn@gmail.com>
2016-11-19 16:39:49 -0200

    Prepare to run next 10-08

--- src/br/inpe/cap/evolution/processor/JoinSummaryCSVPostProcessor.java
+++ src/br/inpe/cap/evolution/processor/JoinSummaryCSVPostProcessor.java
@@ -12,7 +12,7 @@ import br.inpe.cap.evolution.maven.CommitLine;
 
 public class JoinSummaryCSVPostProcessor {
 	
-	private static final String APACHE_FILE_PREFIX = "artifact_aggregation";
+	private static final String APACHE_FILE_PREFIX = "all_dependency_detector";
 	private static final String STUDY_LOG_PATH = "." + File.separator + "study" + File.separator + APACHE_FILE_PREFIX;
 	private static final String EVOLUTION_LOG_PATH = STUDY_LOG_PATH + File.separator + "evolutions";
 	private static final File OUTPUT = new File(EVOLUTION_LOG_PATH + "_joined.csv");
@@ -21,7 +21,7 @@ public class JoinSummaryCSVPostProcessor {
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Starting...");
-		new JoinSummaryCSVPostProcessor(true).process(EVOLUTION_LOG_PATH, OUTPUT, ArtifactAggregatorPostProcessor.HEADER);
+		new JoinSummaryCSVPostProcessor(true).process(EVOLUTION_LOG_PATH, OUTPUT, CommitLine.HEADER);
 		System.out.println("Finish!");
 	}
 
