a41c2bcebaa319f7cb851411142790714ff685db
Luiz Wagner <nascimentolwtn@gmail.com>
2017-04-03 23:08:30 -0300

    Parse de Header com caractere invisível na posição 0.

--- test/br/inpe/cap/evolution/maven/CommitLineTest.java
+++ test/br/inpe/cap/evolution/maven/CommitLineTest.java
@@ -19,6 +19,7 @@ public class CommitLineTest {
 	
 	private String header = CommitLine.OUTPUT_HEADER;
 	private String wrongHeader = CommitLine.WRONG_OUTPUT_HEADER;
+	private String invisibleCharHeader = CommitLine.INVISIBLE_CHAR0_OUTPUT_HEADER;
 	private String line = "0e60b7d9888217be8499fe8b13c7b55e707f269c,10/05/2011 22:35:21,druid,E:\\metricminer-evolution-stars\\druid\\pom.xml,2,4890,0.04090,false,com.alibaba.external,java.servlet,2.5,Share project 'druid' into 'http://code.alibabatech.com/svn/druid'git-svn-id: http://code.alibabatech.com/svn/druid/trunk@2 b9813039-fb51-4c41-a8b9-e21c2acb5095";
 	private String outputLine = "c425502f5c82f49535f9e11a7038cf3df11c29b1,31/05/2014 15:22:45,disconf,E:\\metricminer-evolution-stars\\disconf\\pom.xml,2,1154,0.17331,org.reflections,reflections,0.9.9-RC1,initial_version,false,false,first pack";
 	private String lineWithoutMessage = "6a6f3d45a521518eec4d8531ec9d8534a3f17bf3,22/07/2013 08:59,Activiti,E:\\metricminer-evolution-stars\\Activiti\\modules\\activiti-bpmn-converter\\pom.xml,3989,7530,52.97477,false,org.activiti,activiti-bpmn-model,5.14-SNAPSHOT,";
@@ -124,6 +125,17 @@ public class CommitLineTest {
 	public void invalidParseWrongHeader() {
 		try {
 			CommitLine.parseCommitLine(wrongHeader, CommitLineType.INPUT);
+			fail("Shoud throw Unparse Exception");
+		} catch (RuntimeException e) {
+			Assert.assertSame(NonParseableCommitLineException.class, e.getClass());
+			assertEquals("Should not parse the CSV header.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void invalidParseInvisibleCharHeader() {
+		try {
+			CommitLine.parseCommitLine(invisibleCharHeader, CommitLineType.OUTPUT);
 			fail("Shoud throw Unparse Exception");
 		} catch (RuntimeException e) {
 			Assert.assertSame(NonParseableCommitLineException.class, e.getClass());
